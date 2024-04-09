de97d3951b6901d798cf6ecea82d682298e73d47
yangjinyu2023 <141023330+yangjinyu2023@users.noreply.github.com>
2024-04-09 16:33:04 +0800

    Delete target directory

--- target/generated-sources/annotations/com/example/demo/jmh_generated/StringConnectTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.jmh_generated;
-public class StringConnectTest_jmhType extends StringConnectTest_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/com/example/demo/test/jmh_generated/StringConnectTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.test.jmh_generated;
-public class StringConnectTest_jmhType extends StringConnectTest_jmhType_B3 {
-}
-
